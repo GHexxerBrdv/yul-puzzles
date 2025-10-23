364371f94907383a51ba747d5f4b050c1b8b36bb
GHexxerBrdv <gaurang1415@gmail.com>
2025-10-24 00:43:26 +0530

    Revert With Selector Plus Args

--- src/RevertWithSelectorPlusArgs.sol
+++ src/RevertWithSelectorPlusArgs.sol
@@ -10,6 +10,22 @@ contract RevertWithSelectorPlusArgs {
             // revert custom error with x parameter
             // Hint: concatenate selector and x by storing them
             // adjacent to each other in memory
+            // 
+            // To store this error in EVM memory we need
+            // 1. Error selecotr -> 0xae412287
+            // 2. Abi encoded parameters 
+            // 
+            // uint256 is encoded in 32 bytes big-endian word
+            
+            let x_val := calldataload(4) // loading the value of x from calldata
+            
+            let ptr := mload(0x40) // free memory pointer
+            
+            mstore(ptr, 0xae41228700000000000000000000000000000000000000000000000000000000) // store the error selector to the free memory pointer
+            
+            mstore(add(ptr, 0x04), x_val) // store the value immidiatly after the selector
+            
+            revert(ptr, 0x24)
         }
     }
 }
