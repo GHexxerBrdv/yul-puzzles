e97f5e60dc1f3065167ff3193cf160a857b27290
GHexxerBrdv <gaurang1415@gmail.com>
2025-10-24 13:21:28 +0530

    Double Or Nothing

--- src/DoubleOrNothing.sol
+++ src/DoubleOrNothing.sol
@@ -9,7 +9,14 @@ contract DoubleOrNothing {
             // 21 or less. If 2 * x > 21, then
             // return 0.
             // Hint: check if x ≤ 10, which is equivalent to x < 11
-
+            
+            let ptr := mload(0x40)
+            let result
+            if lt(x, 11) {
+                result := mul(2, x)
+            }
+            mstore(ptr, result)
+            return(ptr, 0x20)
         }
     }
 }
