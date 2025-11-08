020e4185dcf681209d2a5a2d0a1f48e3bbf1b0d0
GHexxerBrdv <gaurang1415@gmail.com>
2025-11-08 13:46:45 +0530

    Write To Fixed Array

--- src/WriteToFixedArray.sol
+++ src/WriteToFixedArray.sol
@@ -9,6 +9,13 @@ contract WriteToFixedArray {
             // your code here
             // store the values in the fixed array `x` in the storage variable `writeHere`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            
+            let slot := writeHere.slot
+            // let length := mload(x) Fixed size memory does not store the length as dynamic do
+            for {let i := 0} lt(i, 5) {i := add(i, 1)} {
+                let value := mload(add(x, mul(i, 0x20)))
+                sstore(add(slot, i), value)
+            }
         }
     }
 
