97490b6ea505c3170c4b41c6cb97a56899048930
GHexxerBrdv <gaurang1415@gmail.com>
2025-11-09 12:18:10 +0530

    Write To Dynamic Array

--- src/WriteToDynamicArray.sol
+++ src/WriteToDynamicArray.sol
@@ -9,6 +9,19 @@ contract WriteToDynamicArray {
             // your code here
             // store the values in the DYNAMIC array `x` in the storage variable `writeHere`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            let ptr := mload(0x40)
+            let length := mload(x)
+            let slot := writeHere.slot
+            sstore(slot, length)
+            mstore(ptr, slot)
+            
+            let dataSlot := keccak256(ptr, 0x20)
+            
+            for {let i := 0} lt(i, length) {i := add(i, 1)} {
+                let value := mload(add(add(x, 0x20), mul(i, 0x20)))
+                
+                sstore(add(dataSlot, i), value)
+            }
         }
     }
 
