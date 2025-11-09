56e3301ab335dda4cefb67a4ccbb9eed0ff7f9de
GHexxerBrdv <gaurang1415@gmail.com>
2025-11-09 19:42:25 +0530

    Write To Mapping

--- src/WriteToMapping.sol
+++ src/WriteToMapping.sol
@@ -9,6 +9,13 @@ contract WriteToMapping {
             // your code here
             // store `value` at the `index` in the mapping `writeHere`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            
+            let ptr := mload(0x40)
+            let slot := writeHere.slot
+            mstore(ptr, index)
+            mstore(add(ptr, 0x20), slot)
+            let dataSlot := keccak256(ptr, 0x40)
+            sstore(dataSlot, value)
         }
     }
 }
