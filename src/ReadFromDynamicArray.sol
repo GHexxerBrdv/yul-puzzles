f013541faefb88e266dc2fc17a5c203589e61a86
GHexxerBrdv <gaurang1415@gmail.com>
2025-11-09 11:55:35 +0530

    Read From Dynamic Array

--- src/ReadFromDynamicArray.sol
+++ src/ReadFromDynamicArray.sol
@@ -15,6 +15,14 @@ contract ReadFromDynamicArray {
             // and return it
             // Assume `index` is <= to the length of readMe
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            let ptr := mload(0x40)
+            let slot := readMe.slot
+            mstore(ptr, slot)
+            let dataSlot := keccak256(ptr, 0x20)
+            let elementSlot := add(dataSlot, index)
+            let value := sload(elementSlot)
+            mstore(ptr, value)
+            return(ptr, 0x20)
         }
     }
 }
