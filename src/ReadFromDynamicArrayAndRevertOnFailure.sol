083ddb5f532ed0347bde6e5f376dc22355a499f2
GHexxerBrdv <gaurang1415@gmail.com>
2025-11-10 15:14:15 +0530

    Read From Dynamic Array And Revert On Failure

--- src/ReadFromDynamicArrayAndRevertOnFailure.sol
+++ src/ReadFromDynamicArrayAndRevertOnFailure.sol
@@ -15,6 +15,21 @@ contract ReadFromDynamicArrayAndRevertOnFailure {
             // and return it
             // Revert with Solidity panic on failure, use error code 0x32 (out-of-bounds or negative index)
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            
+            let baseSlot := readMe.slot
+            let len := sload(baseSlot)
+            let ptr := mload(0x40)
+            if or(lt(index, 0), iszero(lt(index, len))) {
+                mstore(ptr, shl(224, 0x4e487b71))
+                mstore(add(ptr, 0x04), 0x32)
+                revert(ptr, 0x24)
+            }
+            
+            mstore(ptr, baseSlot)
+            let dataSlot := keccak256(ptr, 0x20)
+            let value := sload(add(dataSlot, index))
+            mstore(ptr, value)
+            return(ptr, 0x20)
         }
     }
 }
