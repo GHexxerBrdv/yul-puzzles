ff7c09df55754fd8eaa4cd4e8d940da6a345a1b2
GHexxerBrdv <gaurang1415@gmail.com>
2025-11-10 15:18:25 +0530

    Write To Double Mapping

--- src/WriteToDoubleMapping.sol
+++ src/WriteToDoubleMapping.sol
@@ -9,6 +9,16 @@ contract WriteToDoubleMapping {
             // your code here
             // set the `value` for a `user` and a `token`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            
+            let ptr := mload(0x40)
+            let baseSlot := balances.slot
+            mstore(ptr, user)
+            mstore(add(ptr, 0x20), baseSlot)
+            let initialHash := keccak256(ptr, 0x40)
+            mstore(ptr, token)
+            mstore(add(ptr, 0x20), initialHash)
+            let dataSlot := keccak256(ptr, 0x40)
+            sstore(dataSlot, value)
         }
     }
 }
