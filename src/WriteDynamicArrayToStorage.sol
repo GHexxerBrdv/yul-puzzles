fef8e2a566ce4efa01aced99980fb1b16f5cab0f
GHexxerBrdv <gaurang1415@gmail.com>
2025-11-11 13:11:22 +0530

    Write Dynamic Array To Storage

--- src/WriteDynamicArrayToStorage.sol
+++ src/WriteDynamicArrayToStorage.sol
@@ -8,6 +8,23 @@ contract WriteDynamicArrayToStorage {
         assembly {
             // your code here
             // write the dynamic calldata array `x` to storage variable `writeHere`
+            
+            let offset := calldataload(4)
+            let dataPos := add(4, offset)
+            
+            let len := calldataload(dataPos)
+            let elSlot := add(dataPos, 0x20)
+            let slot := writeHere.slot
+            sstore(slot, len)
+            
+            let ptr := mload(0x40)
+            mstore(ptr, slot)
+            let dataSlot := keccak256(ptr, 0x20)
+            
+            for {let i := 0} lt(i, len) {i := add(i, 1)} {
+                let value := calldataload(add(elSlot, mul(i, 0x20)))
+                sstore(add(dataSlot, i), value)
+            }
         }
     }
 }
