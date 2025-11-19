90b48f63dc318ff9a75e7709f230e522031a9615
GHexxerBrdv <gaurang1415@gmail.com>
2025-11-19 15:47:30 +0530

    Emit Event With Indexed And Multiple Data

--- src/EventWithIndexedData.sol
+++ src/EventWithIndexedData.sol
@@ -15,6 +15,10 @@ contract EventWithIndexedData {
             //  topic 1: The `emitter` address
             //  topic 2: The `id` bytes32 value
             // include the `number` field as the data payload
+            
+            let ptr := mload(0x40)
+            mstore(ptr, num)
+            log3(ptr, 0x20, 0x044d482819499c9d5fde1245ce63873b1259fc52fc78651ccdcdf7392637d374, emitter, id)
         }
     }
 }
